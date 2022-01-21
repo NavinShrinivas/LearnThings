d8666607c14100fdb00268be3e7b70602f93c6f0
P K Navin Shirnivas <karupal2002@gmail.com>
2022-01-21 22:49:32 +0530

    module08 over
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>
    
    Man, Cus of rust I can now see errors in c and c++ code, kinda freaky
    anyways slices refrence ownership borrow, all these were hard. We def
    have to do one revision scene. Lemmee look into it

--- Rust-02/module08/src/main.rs
+++ Rust-02/module08/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+use std::io::prelude::*;
 
 fn oldwordfn(s : &String)->usize{
     let bytes = s.as_bytes();
@@ -23,15 +25,35 @@ fn newwordfn(s : &String)->&str{
 
 }
 
+fn somefn(s: &String)->&str{
+    &s[..3]
+}
+
 
 fn main() {
     println!("Hello, world!");
-    let s : String = String::from("hwllo world");
-    let space = oldwordfn(&s);
+    let mut s : String = String::from("hmmmm");
+    //let space = oldwordfn(&s);
     //say we did s.clear herem space would still have a value that makes sense to previous value of
     //string, this is tedious to track , hence slices.
     //slices are redable refrence only
-    let slice = newwordfn(&s);
+    print!("Enter a senctence : ");
+    io::stdout().flush().expect("Something went wrong");
+    let re = somefn(&s);
+    io::stdin().read_line(&mut s).expect("Something went wrong");
+    //s.clear();//no error
+    let slice = newwordfn(&s);//does not give erro , even thos there already exists a mutablle borroe, this is cus
+    //println!("{}",re); //gives error
+
+    //FINALLY : stuff is made clear, you can not use a immutable refrence that was created before a
+    //mutable borrw or refrence was made.
+
+
+
+
+    //rust can tell that the mutable borrow is not used anymore and its safe to have this immutable
+    //stuff, we can also indeeed use a s.clear before immutable ref
     //s.clear();//gives error,as we are using are creating a mutable refrence when a readable
     //refrence exists
+    println!("{}",slice);
 }
