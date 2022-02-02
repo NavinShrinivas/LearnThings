55f139feeea604e6361b1cfd2568b510e7b919af
P K Navin Shirnivas <karupal2002@gmail.com>
2022-02-02 22:10:01 +0530

    little about strings and vectors
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>
    
    Yeah i slowed down down, iss fine, I'll pick the pace up :)

--- Rust-02/module12/readme.md
+++ Rust-02/module12/readme.md
@@ -14,3 +14,16 @@ borrow checker applies here as well, no mutable borrows when immutable borrows a
 
 Vectors can normally only store single type of data, but with use of enums we can store different types in a single 
 array.
+
+## Strings 
+
+String are a Vector of bits in Rust and store any valid utf-8 encoded string, it has a "push_str" method which takes a
+mutable reference, it also has "push" method.
+
+Concatenation with the + operator is the tricky one, it take one String and one &str, moves the String to new String 
+and uses immutable reference of &str to add value!.
+
+But this way of concatenation is very messy when you want to start doing more complex stuff, hence we can use the
+format! macro. Format macro is very similar to our println! except it returns a values back.
+
+
--- Rust-02/module12/src/main.rs
+++ Rust-02/module12/src/main.rs
@@ -1,9 +1,10 @@
-enum Heterogenous{
+use std::fmt::format;
+
+enum Heterogenous {
     Text(String),
     Number(i32),
-    Float(f64)
+    Float(f64),
 }
-
 
 fn main() {
     println!("Hello, world!");
@@ -17,23 +18,38 @@ fn main() {
                                         *second_vec.push(4);//not allowed
                                         */
     println!("Third element : {} {}", third, third2);
-    match second_vec.get(3){
-        Some(value) => println!("4th values in the vector : {}",value),
-        None => println!("Trying to access invalid index, haha you can't break me!")
+    match second_vec.get(3) {
+        Some(value) => println!("4th values in the vector : {}", value),
+        None => println!("Trying to access invalid index, haha you can't break me!"),
     }
     println!("Contents of the array : ");
     for (i, item) in second_vec.iter().enumerate() {
         println!(" {}th element is {}", i, item);
     }
-    let mut typ : Vec<Heterogenous> = Vec::new();
+    let mut typ: Vec<Heterogenous> = Vec::new();
     typ.push(Heterogenous::Number(6));
     typ.push(Heterogenous::Text(String::from("Hello world")));
     typ.push(Heterogenous::Float(3.4));
-    for (_i,item) in typ.iter().enumerate(){
-        match item{
-            Heterogenous::Number(value) => println!("Found a number : {}",value),
-            Heterogenous::Text(value) => println!("Found text : {}",value),
-            Heterogenous::Float(value)=> println!("Floating point number : {}",value)
+    println!("Contents of heterogenous vector");
+    for (_i, item) in typ.iter().enumerate() {
+        match item {
+            Heterogenous::Number(value) => println!("Found a number : {}", value),
+            Heterogenous::Text(value) => println!("Found text : {}", value),
+            Heterogenous::Float(value) => println!("Floating point number : {}", value),
         }
     }
+    println!("Content of array!");
+    for i in &second_vec {
+        println!("content of array: {}", i);
+    }
+    let mut new_str: String = String::from("haiii,");
+    new_str.push_str("hello world");
+    println!("String: {}", new_str);
+    let pstr = String::from("haiiii");
+    let sec_str = new_str + &pstr; //Rust is smart and converts &String to &str
+                                   //here onwards new_str is moved, pstr is will valid!
+                                   //println!("new_str : {}",new_str);//not allowe
+    println!("sec_str : {} |||| pstr : {}", sec_str, pstr); //valid
+    let str3 = format!("{}-+=[{}]+{}", pstr, pstr, pstr); //only immutable references
+    println!("{} {}", str3, pstr); //allowed
 }
