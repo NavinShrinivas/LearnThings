37e2103a3548f21646c4a2c01f83c781131770b9
P K Navin Shirnivas <karupal2002@gmail.com>
2022-03-04 17:52:57 +0530

    FINALLY SOME PROGRESS
    Signed-off-by: P K Navin Shirnivas <karupal2002@gmail.com>
    
    The Block has been broken, I should be able to raise the effort easily
    now :)

--- Rust-02/module15/readme.md
+++ Rust-02/module15/readme.md
@@ -5,3 +5,22 @@
 If I understood right from the book, generic types are polymorphism without OOPS concepts.
 
 The first function we write using a generic type is the largest on an array irrespective of the type.
+Generic Types (T) do have traits such as comparision, aritemetic, blah blah implmented. Its ofc the programmers responsbility, this work of defining traits (If your are coming from OOPS background the word "method" will resonate better).
+
+above doesnt work, as there can be onyl one impl with one name, but it can be restricted through generic types.
+
+
+You might be wondering whether there is a runtime cost when you’re using generic type parameters. The good news is that Rust implements generics in such a way that your code doesn’t run any slower using generic types than it would with concrete types.
+
+Rust accomplishes this by performing monomorphization of the code that is using generics at compile time. Monomorphization is the process of turning generic code into specific code by filling in the concrete types that are used when compiled.
+
+Stupidly beautiful rust, MAH GOD it never stops to amaze me. 
+
+## Traits 
+So we have seen how to bound impls using specific types, with what i have initially understood. We can define few methods for generics that have a particular trait.
+
+While going through book try and relate traits and behviours (methods) to humans and it made much more sense to me.
+
+Coming from C and C++ this traits felt stupidly similar to 
+
+Same behviour names can be used across traits but ofc not within, We can also give behaviour traits default fns.
--- Rust-02/module15/src/main.rs
+++ Rust-02/module15/src/main.rs
@@ -1,13 +1,143 @@
-fn generic_largest<T>(list: &[T])-> T{
-    let mut largest = list[0];
+struct Coords<T> {
+    x: T,
+    y: T,
+    z: i32,
+}
 
-    for i in list.iter(){
-        if(i > largest)
-            { largest = i; }
+impl<T> Coords<T> {
+    fn printf(&self) {
+        println!("{}", std::any::type_name::<T>());
     }
 }
+
+impl Coords<i32> {
+    /*
+     *fn printf(&self) {
+     *    println!("The impl for i32 has bee called");
+     *}
+     */
+    //above doesnt work, as there can be onyl one impl with one name, but it can be restricted
+    //through generic types.
+    fn print32(&self) {
+        println!("The impl for i32 has bee called");
+}
+}
+
+/*
+ *fn generic_largest<T>(list: &[T]) -> T {
+ *    let mut largest = list[0];
+ *
+ *    for i in list {
+ *        if *i > largest {
+ *            //lets get to correting this error later
+ *            largest = *i;
+ *        }
+ *    }
+ *    largest
+ *}
+ */
+
+
+// Traits : 
+ 
+
+pub trait Sad{
+    fn hit(&self){
+        println!("Generic response of sadness");
+    }
+     fn cry(&self){
+        println!("Generic response of sadness");
+    }
+     fn moody(&self){
+        println!("Generic response of sadness");
+    }
+
+
+
+}
+
+pub trait Happy{
+    fn hit(&self);
+    fn cry(&self);
+    fn moody(&self);
+}
+
+struct Kid{
+    name : String
+}
+
+struct Adult{
+    name : String
+}
+
+struct OldPeople{
+    name : String
+}
+
+struct Dead{
+    name : String
+}
+
+impl Happy for Kid{
+   fn hit(&self){
+        println!("{} nenver hit people cus they happy",self.name);
+    }
+    fn cry(&self){
+        println!("{} cry cus he/she/others upset at times",self.name);
+    }
+    fn moody(&self){
+        println!("{} never be moody",self.name);
+    }
+//every fucntion in the trait must be implemented
+} 
+
+
+impl Sad for Adult{
+    fn hit(&self){
+        println!("{} hit people cus they sad :(",self.name);
+    }
+    fn cry(&self){
+        println!("{} cry cus he/she/others Sad",self.name);
+    }
+    fn moody(&self){
+        println!("{} always be moody",self.name);
+    }
+//every fucntion in the trait must be implemented
+}
+
+impl Sad for Dead{ //enuf for getting default behaviours of the trait into scope.
+}
+
+
+
+
 
 
 fn main() {
     println!("Hello, world!");
+    let coordinate1: Coords<i32> = Coords {
+        x: 12,
+        y: 13,
+        z: 15,
+    };
+    //let coordinate2 = Coords { x : 13 , y :13.5 , z : 78 }; //not allowed
+    let coordinate2 = Coords {
+        x: 30.5,
+        y: 40.0,
+        z: 45,
+    };
+    coordinate2.printf();
+    coordinate1.printf();
+    coordinate1.print32(); //works
+                           //coordinate2.print32(); //doesnt work :)
+    // Traits : 
+    let kid1 = Kid{name : "Baby 2343".to_string()};
+    let adult1 = Adult {name : "Adult 53452".to_string()};
+    let _old1 = OldPeople {name : "Old person 23423".to_string()};
+    let dead1 = Dead { name : String::from("Dead 2321") };
+
+    kid1.hit();
+    adult1.hit();
+    //old1.hit(); //wont work
+    dead1.cry();
 }
