3e2047bd97e58af4b5dcaf4aa3e9ee9c4efc047e
P K Navin Shirnivas <karupal2002@gmail.com>
2022-03-24 08:25:50 +0530

    rustlings functions

--- Rust-02/Rustlings/exercises/functions/functions1.rs
+++ Rust-02/Rustlings/exercises/functions/functions1.rs
@@ -1,7 +1,11 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
+
+fn call_me(){
+    println!("Something");
+}
 
 fn main() {
     call_me();
