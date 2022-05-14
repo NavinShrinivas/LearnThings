37ef74eb484d8a1177ef2f3a13bc3d6b19408902
NavinShrinivas <karupal2002@gmail.com>
2022-05-14 07:30:41 +0530

    little on rust

--- Rust-FastPaced/Enums-03/src/main.rs
+++ Rust-FastPaced/Enums-03/src/main.rs
@@ -1,3 +1,31 @@
+//This moduel is all abotu enums and options
+#[derive(Debug)]
+struct Day{
+    day_in_words: String,
+    date: i32,
+    month: i32,
+    year: i32
+}
+#[derive(Debug)]
+enum DayType{
+    Weekday(Day),
+    Weekend(Day)
+}
+
+impl DayType{
+    fn print_pretty(self: &Self){
+        println!("{:#?}",self);
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let new_day = DayType::Weekday(Day{
+        day_in_words : String::from("Tuesday"),
+        date:12,
+        month:21,
+        year:2022
+    });
+    new_day.print_pretty();
 }
