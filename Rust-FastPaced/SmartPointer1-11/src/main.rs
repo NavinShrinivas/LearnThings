8bba4ff5c6211351b5226adebf729af3ef420712
NavinShrinivas <karupal2002@gmail.com>
2022-06-24 10:34:34 +0530

    Forgot to add to box

--- Rust-FastPaced/SmartPointer1-11/src/main.rs
+++ Rust-FastPaced/SmartPointer1-11/src/main.rs
@@ -3,6 +3,7 @@
  *Cons list is from lisp language and is a recursive data structure
  *The concept of Cons is explained in the rust book
  */
+#[derive(Debug)]
 enum List{
     /*
      *Cons(i32,List),//Wont work, this is cus rust cant figure out how much space to allocate for List
@@ -12,12 +13,14 @@ enum List{
     Nil
 }
 
-
+use crate::List::{Cons,Nil};
 
 fn main() {
     let first_box : Box<u32> = Box::new(9834587);
     println!("{}",first_box); //meaning Box types have fmt print impl
                             
     //recursive types in boxes 
+    let psuedo_linked_list : List = Cons(12,Box::new(Cons(23,Box::new(Cons(34,Box::new(Nil))))));
+    println!("{:#?}",psuedo_linked_list);
 
 }
